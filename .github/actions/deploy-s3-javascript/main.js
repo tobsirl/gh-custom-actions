58d6a2bc76a43edd534d01af98f33d21ec49cf1e
Paul Tobin <tobster111@gmail.com>
2023-12-20 11:36:32 +0000

    Github Actions - js action

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -3,6 +3,14 @@ const github = require("@actions/github");
 const exec = require("@actions/exec");
 
 async function run() {
+  // Get inputs
+  const bucket = core.getInput("bucket", { required: true });
+  const bucketRegion = core.getInput("bucket-region", { required: true });
+  const distFolder = core.getInput("dist-folder", { required: true });
+
+  // Upload files to S3
+  const s3Uri = `s3://${bucket}`;
+  exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
   core.notice("Deploying to S3");
 }
 
